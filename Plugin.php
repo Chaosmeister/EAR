0d182b9e18b917bed336d8b48f8db4ba5395653c
Tomas Dittmann <chaosmeist3r@gmail.com>
2021-11-23 13:56:50 +0100

    add plugin homepage

--- Plugin.php
+++ Plugin.php
@@ -39,4 +39,9 @@ class Plugin extends Base
     {
         return '1.0.0';
     }
+    
+    public function getPluginHomepage()
+    {
+        return "https://github.com/Chaosmeister/EAR"
+    }
 }
